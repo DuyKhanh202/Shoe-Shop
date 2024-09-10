3dbedfd1d54f5bcc82a8ab47ee39aa88f0d46b75
DuyKhanh202 <lenguyenduykhanh02@gmail.com>
2024-09-11 00:07:30 +0700

    update code Shoe Shop

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Ex_ShoeShop from "./Ex_ShoeShop/Ex_ShoeShop";
+function App() {
+  return (
+    <div>
+      <Ex_ShoeShop />
+    </div>
+  );
+}
+
+export default App;
